df6aeeb2fc0e2dce05d56829d63b466a32646dde
Provash <provash20cb@gmail.com>
2026-09-05 15:49:20 +0600

    41-7 Implementing Required Business Logics Inside Cron Job Scheduler For Deleting Applied Doctors With Unverified Email

--- src/app/lib/cron.ts
+++ src/app/lib/cron.ts
@@ -1,11 +1,38 @@
 import cron from 'node-cron';
+import { DoctorVerificationStatus, Prisma, Role } from '../../generated/prisma/client';
+import { prisma } from './prisma';
 
 
 
 export const deleteUnverifiedfieldDoctors = async () => {
-    cron.schedule('*/2 * * * * *', () => {
+    cron.schedule('*/10 * * * *', async() => {
     //prisma business => doctors delete
     //   console.log('running a task every minute');
-    console.log("Doctor Delete")
+    // console.log("Doctor Delete")
+
+    try {
+        const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000)
+    const deleteDoctors = await prisma.user.deleteMany({
+        where : {
+            role : Role.DOCTOR,
+            emailVerified : false,
+            createdAt : {lt : oneHourAgo},
+            doctor : {
+                verificationStatus : DoctorVerificationStatus.PENDING
+            }
+        }
+    })
+
+    if(deleteDoctors.count > 0){
+        console.log(`
+            Cron:Deleted ${deleteDoctors.count} unverified email doctor applications older than 1 hour`);
+            
+    }
+    } catch (error) {
+        console.log("Cron : Failed to delete unverified doctor application", error)
+    }
+
+    console.log("Unverified Doctor Delete cron schedule every 10 minutes")
+
 });
 }
